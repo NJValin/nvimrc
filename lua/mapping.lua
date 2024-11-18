ed73ae50f1636141c53ba4edfcc3736b63280f6d
NJValin <neilvalin1@gmail.com>
2024-11-17 23:30:02 -0500

    test

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -3,7 +3,7 @@
 vim.g.mapleader = " "
 
 local opts = { noremap = true, silent = true }
--- Key mappings
+-- Key mappinrequire('gitsigns')
 local map = vim.api.nvim_set_keymap
 --map("n", "<leader>ff", ":Telescope find_files<CR>", opts)
 map("n", "<leader>pv", ":Ex<CR>", opts)
@@ -11,9 +11,45 @@ map("i", "jk", "<Esc>", opts)
 map("i", "kj", "<Esc>", opts)
 map("n", ";", ":", opts)
 
--- Telescope plugins
-vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { desc = "Find Files" })
-vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Live Grep" })
-vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find Buffers" })
-vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Find Help" })
+-- Telescope plugin keybinds
+map('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { desc = "Find Files" })
+map("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Live Grep" })
+map("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find Buffers" })
+map("n", "<leader>fh", "<cmd>Telescope help_tarequire('gitsigns')<cr>", { desc = "Find Help" })
 
+-- Gitsigns plugin keybinds
+vim.keymap.set('n', ']c', function() 
+  require('gitsigns').next_hunk()
+end, {desc="Next Git Hunk"})
+
+vim.keymap.set('n', '[c', function() 
+  require('gitsigns').prev_hunk() 
+end, {desc="Previous Git Hunk"})
+
+vim.keymap.set('n', '<leader>hs', function() 
+  require('gitsigns').stage_hunk() 
+end, {desc="Stage Git Hunk"})
+
+vim.keymap.set('n', '<leader>hr', function()
+  require('gitsigns').reset_hunk()
+end, {desc="Reset Git Hunk"})
+
+vim.keymap.set('n', '<leader>hS', function() 
+  require('gitsigns').stage_buffer() 
+end, {desc="Stage entire buffer"})
+
+vim.keymap.set('n', '<leader>hu', function() 
+  require('gitsigns').undo_stage_hunk() 
+end, {desc="Undo Stage Hunk"})
+
+vim.keymap.set('n', '<leader>hp', function() 
+  require('gitsigns').preview_hunk() 
+end, {desc="Preview Git Hunk"})
+
+vim.keymap.set('n', '<leader>hb', function() 
+  require('gitsigns').blame_line() 
+end, {desc='Blame line'})
+
+vim.keymap.set('n', '<leader>hd', function() 
+  require('gitsigns').diffthis() 
+end, {desc='Show diff'})
